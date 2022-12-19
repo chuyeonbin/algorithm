63c2d856524fb2136aa1067db8f696d01bebc653
Buggy <dbgml1007589@naver.com>
2022-12-19 23:50:10 +0900

    kimjihwan.js upload

---
+++ Programmers/2021_카카오_채용연계형_인턴십/숫자_문자열과_영단어/kimjihwan.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    var chanum = s;
+
+    chanum = chanum.replace(/zero/gi, 0);
+    chanum = chanum.replace(/one/gi, 1);
+    chanum = chanum.replace(/two/gi, 2);
+    chanum = chanum.replace(/three/gi, 3);
+    chanum = chanum.replace(/four/gi, 4);
+    chanum = chanum.replace(/five/gi, 5);
+    chanum = chanum.replace(/six/gi, 6);
+    chanum = chanum.replace(/seven/gi, 7);
+    chanum = chanum.replace(/eight/gi, 8);
+    chanum = chanum.replace(/nine/gi, 9);
+
+    console.log(chanum)
+
+    return Number(chanum);
+}
