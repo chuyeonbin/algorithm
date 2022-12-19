81134fabcf35cf2cbadbb73c9f0e2d9def5e24ea
ChuYeonBin <cndusqls98@gmail.com>
2022-12-19 23:36:17 +0900

    프로그래머스/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어

---
+++ Programmers/2021_카카오_채용연계형_인턴십/숫자_문자열과_영단어/chuyeonbin.js
@@ -0,0 +1,33 @@
+function solution(s) {
+  var answer = '';
+  const number = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  var str = '';
+
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] < 10) {
+      answer += s[i];
+      continue;
+    }
+    str += s[i];
+    for (var j = 0; j < number.length; j++) {
+      if (str === number[j]) {
+        answer += j;
+        str = '';
+        break;
+      }
+    }
+  }
+
+  return Number(answer);
+}
