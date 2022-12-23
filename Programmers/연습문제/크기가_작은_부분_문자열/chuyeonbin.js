f194b1fcd4101e790731f3c6d07f2dab36e34bad
ChuYeonBin <cndusqls98@gmail.com>
2022-12-24 01:31:53 +0900

    연습문제/크기가_작은_부분_문자열/chuyeonbin.js

---
+++ Programmers/연습문제/크기가_작은_부분_문자열/chuyeonbin.js
@@ -0,0 +1,14 @@
+function solution(t, p) {
+  var answer = 0;
+
+  var i = 0;
+
+  while (t.slice(i, i + p.length).length === p.length) {
+    console.log(t.slice(i, i + p.length));
+    if (t.slice(i, i + p.length) <= p) {
+      answer++;
+    }
+    i++;
+  }
+  return answer;
+}
