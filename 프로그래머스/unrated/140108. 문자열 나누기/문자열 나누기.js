ca1a9ad3037fe720898747d657f75c3a0c2e0a0d
추연빈 <cndusqls98@gmail.com>
2023-09-12 11:33:36 +0900

    [unrated] Title: 문자열 나누기, Time: 0.69 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,25 @@
+function solution(s) {
+    var answer = 0;
+    var x = s[0];
+    var str = s;
+    var count1 = 1;
+    var count2 = 0;
+    
+    for(var i = 1; i <= s.length; i++) { 
+        if(x === s[i]) {
+            count1++;
+        } else count2++;
+        
+        if(count1 === count2) {
+            str = s.slice(i + 1, s.length);
+            x = str[0];
+            i++;
+            count1 = 1;
+            count2 = 0; 
+            answer++;
+        } else if((answer === 0 && i === s.length) || (i === s.length && count1 > count2)) {
+            answer++;
+        }
+    }
+    return answer;
+}
