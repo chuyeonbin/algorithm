152bcb841f1e41312043eb7cbb1a6526a9aa2dfc
추연빈 <cndusqls98@gmail.com>
2023-09-12 11:32:29 +0900

    [unrated] Title: 둘만의 암호, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,21 @@
+function solution(s, skip, index) {
+    var answer = '';
+
+    
+    for(var i = 0; i < s.length; i++) {
+        var count = 0;
+        for(var j = s.charCodeAt(i) + 1; count < index; j++) {
+            if(j > 122) {
+                j = 97;
+            }
+            
+            if(skip.indexOf(String.fromCharCode(j)) !== -1 ) continue;
+            
+            count++;
+            if(count === index) {
+                answer += String.fromCharCode(j);
+            }
+        }
+    }
+    return answer;
+}
