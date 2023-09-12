86daed1e0dfba907fa51942341358852edb74712
추연빈 <cndusqls98@gmail.com>
2023-09-12 11:33:55 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,11 @@
+function solution(k, m, score) {
+    var answer = 0;
+    
+    var sort = score.sort((a, b) => b - a);
+    
+    for(var i = m; i <= sort.length; i = i + m) {
+        answer += (sort[i - 1] * m);
+    }
+    
+    return answer;
+}
