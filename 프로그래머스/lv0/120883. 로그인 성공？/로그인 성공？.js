23350950c21224a97c3e072f48966f941a095948
추연빈 <cndusqls98@gmail.com>
2023-09-12 11:35:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,16 @@
+function solution(id_pw, db) {
+    var answer = '';
+    
+    for(let i = 0; i < db.length; i++) {
+        if(id_pw[0] !== db[i][0]) {
+            continue;
+        } else {
+            if(id_pw[1] !== db[i][1]) {
+                return answer = 'wrong pw';
+            } else {
+                return answer = 'login';
+            }
+        }
+    }
+    return answer = 'fail';
+}
